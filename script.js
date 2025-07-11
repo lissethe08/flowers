539654a54182db8144d323c2959ab1f52c34fd40
lisi <Estefanialopezcevallos@gmail.com>
2025-07-11 16:48:55 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("abrir").addEventListener("click", () => {
+  document.querySelector(".pantalla-inicial").style.display = "none";
+  document.getElementById("flor").style.display = "block";
+});
